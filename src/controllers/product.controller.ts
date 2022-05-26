08bdc19aebec00716e835765dfba5b70ab8015d9
Clinton Mainya <klyntonmainya@gmail.com>
2022-05-27 00:32:06 +0300

    Fix product does not exist bug

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -39,7 +39,7 @@ export async function updateProductHandler(
     res.status(404).send("Product does not exist");
   }
 
-  if (product?.user !== userId) {
+  if (String(product!.user) !== userId) {
     return res.sendStatus(403);
   }
 
@@ -78,7 +78,7 @@ export async function deleteProductHandler(
     res.status(404).send("Product does not exist");
   }
 
-  if (product?.user !== userId) {
+  if (product!.user !== userId) {
     return res.sendStatus(403);
   }
 
