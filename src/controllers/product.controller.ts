505bd0d0b708fbdab737dac8f8f362526e8aea97
Clinton Mainya <klyntonmainya@gmail.com>
2022-05-27 00:03:17 +0300

    Connect the product service with respective endpoint

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,88 @@
+import {
+  CreateProductInput,
+  UpdateProductInput,
+  ReadProductInput,
+  DeleteProductInput,
+} from "./../schemas/product.schema";
+import { Request, Response } from "express";
+import {
+  createProduct,
+  deleteProduct,
+  findAndUpdateProduct,
+  findProduct,
+} from "../services/product.service";
+
+export async function createProductHandler(
+  req: Request<{}, {}, CreateProductInput["body"]>,
+  res: Response
+) {
+  const userId = res.locals.user._id;
+
+  const body = req.body;
+
+  const Product = await createProduct({ ...body, user: userId });
+  return res.send(Product);
+}
+
+export async function updateProductHandler(
+  req: Request<UpdateProductInput["params"]>,
+  res: Response
+) {
+  const userId = res.locals.user._id;
+
+  const productId = req.params.productId;
+  const update = req.body;
+
+  const product = await findProduct({ productId });
+
+  if (!product) {
+    res.status(404).send("Product does not exist");
+  }
+
+  if (product?.user !== userId) {
+    return res.sendStatus(403);
+  }
+
+  const updatedProduct = await findAndUpdateProduct({ productId }, update, {
+    new: true,
+  });
+
+  return res.send(updatedProduct);
+}
+
+export async function getProductHandler(
+  req: Request<ReadProductInput["params"]>,
+  res: Response
+) {
+  const productId = req.params.productId;
+  const product = await findProduct({ productId });
+
+  if (!product) {
+    return res.sendStatus(404);
+  }
+
+  return res.send(product);
+}
+
+export async function deleteProductHandler(
+  req: Request<DeleteProductInput["params"]>,
+  res: Response
+) {
+  const userId = res.locals.user._id;
+
+  const productId = req.params.productId;
+
+  const product = await findProduct({ productId });
+
+  if (!product) {
+    res.status(404).send("Product does not exist");
+  }
+
+  if (product?.user !== userId) {
+    return res.sendStatus(403);
+  }
+
+  await deleteProduct({ productId });
+
+  return res.sendStatus(200);
+}
