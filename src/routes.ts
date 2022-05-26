afb026281508821c471326507ecb2a6fc0a0b1ea
Clinton Mainya <klyntonmainya@gmail.com>
2022-05-27 00:31:32 +0300

    Add product routes with respective CRUD functionality

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,9 @@
+import {
+  getProductSchema,
+  createProductSchema,
+  updateProductSchema,
+  deleteProductSchema,
+} from "./schemas/product.schema";
 import { Express, Request, Response } from "express";
 import {
   createUserSessionHandler,
@@ -9,6 +15,12 @@ import requireUser from "./middlewares/requireUser";
 import validateRequest from "./middlewares/validateRequest";
 import { createSessionSchema } from "./schemas/session.schema";
 import { createUserSchema } from "./schemas/user.schema";
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "./controllers/product.controller";
 
 export default function (app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => {
@@ -27,13 +39,40 @@ export default function (app: Express) {
       validateRequest(createSessionSchema),
       createUserSessionHandler
     );
-
     //Get the user's sessions GET -- /api/sessions
-
     app.get("/api/sessions", requireUser, getUserSessionsHandler);
 
     //Logout --- DELETE /api/sessions
-
     app.delete("/api/sessions", requireUser, deleteSessionsHandler);
+
+    //Create product POST -- /api/products
+
+    app.post(
+      "/api/products",
+      [requireUser, validateRequest(createProductSchema)],
+      createProductHandler
+    );
+
+    //Update product PUT -- /api/products/:productId
+    app.put(
+      "/api/products/:productId",
+      [requireUser, validateRequest(updateProductSchema)],
+      updateProductHandler
+    );
+
+    //Get Product GET -- /api/products
+
+    app.get(
+      "/api/products/:productId",
+      validateRequest(getProductSchema),
+      getProductHandler
+    );
+
+    //Delete Products DELETE -- /api/products/:productId
+    app.delete(
+      "/api/products/:productId",
+      [requireUser, validateRequest(deleteProductSchema)],
+      deleteProductHandler
+    );
   });
 }
